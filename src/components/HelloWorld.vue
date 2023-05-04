e856a99dd07ea7ec19829dabb6a7b1326998cf58
ChrisBlassiaux <chrisblassiaux@gmail.com>
2023-05-04 17:01:03 +0200

    develop about the universe

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,21 @@
     <p>Vous êtes passionné.e de {{ hobbies.art }}</p>
     <button @click="mode($event)">Mode dark / light</button>
     <input type="text" :value="description">
+
+    <!-- Exercice 1 -->
+    <h3>Exo1</h3>
+    <input type="text" @input="changeColor($event)">
+    <div class="square" :style="{ backgroundColor: squareColor }"></div>
+    
+    <!-- Exercice 2 -->
+    <h3>Exo2</h3>
+    <button @click="showHello = !showHello">Apparition/disparition du Hello!</button>
+    <div class="hello" v-if="showHello">Hello !</div>
+
+    <!-- Exercice 3 -->
+    <ul>
+      <li v-for="user in users" :key="user">{{ user.firstName }} {{ user.lastName }}</li>
+    </ul>
   </div>
 </template>
 
@@ -12,6 +27,7 @@
 export default {
   name: 'HelloWorld',
   props: {},
+  // Les données du composant
   data() {
     return {
       name: 'Marina',
@@ -20,17 +36,41 @@ export default {
       hobbies: {
         art: 'Peinture'
       },
-      description: 'Bonjour, je m\'appelle...'
+      description: 'Bonjour, je m\'appelle...',
+      // Exercice 1 
+      squareColor: 'grey',
+      // Exercice 2 
+      showHello: true,
+      // Exercice 3
+      users: [
+        {
+          firstName: 'chris',
+          lastName: 'b'
+        },
+        {
+          firstName: 'chris',
+          lastName: 'b'
+        },
+        {
+          firstName: 'chris',
+          lastName: 'b'
+        }
+      ]
     }
   },
+  // Les méthodes du composant
   methods: {
     mode(event) {
       console.log(event);
       document.body.classList.toggle('dark');
+    },
+    // Exercice 1
+    changeColor(event) {
+      this.squareColor = event.target.value;
     }
   },
+  // Méthode qui s'exécute à l'utilisation du composant
   mounted() {
-    console.log('Salut!');
   }
 }
 </script>
@@ -40,5 +80,12 @@ export default {
 .color-2 {
   background-color: blueviolet;
 }
+.square {
+  height: 200px;
+  width: 200px;
+}
 
+.hello {
+  background-color: red;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="navbar-height"></div>
+    <div class="navbar">
+        <div class="contain-burger">
+            <div class="burger"></div>
+        </div>
+        <nav class="nav">
+            <img src="../assets/logo.png" alt="Logotype" />
+            <router-link to="/" class="navbar-link active">Accueil</router-link>
+        </nav>
+        </div>
+        <nav class="vertical-navbar">
+        <router-link to="/planets" class="nav-vertical-link">
+            <span class="fas fa-globe fa-3x"></span>
+            <p>Planètes</p>
+        </router-link>
+        <a href="#" class="nav-vertical-link">
+            <span class="fas fa-rocket fa-3x"></span>
+            <p>Véhicules</p>
+        </a>
+        <a href="#" class="nav-vertical-link">
+            <span class="fab fa-reddit-alien fa-3x"></span>
+            <p>Personnes</p>
+        </a>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: {},
+    // Les données du composant
+    data() {
+        return {
+            
+        }
+    },
+    // Les méthodes du composant
+    methods: {
+
+    },
+    // Méthode qui s'exécute à l'utilisation du composant
+    mounted() {
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
